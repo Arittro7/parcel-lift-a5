b58d619b5602a5682a59bd6a20cdbac19ea1aae1
Arittro7 <arittrowpdev@gmail.com>
2025-11-13 00:31:39 +0600

    build 6

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -20,10 +20,10 @@ interface EnvConfig {
   FRONTEND_URL: string;
 
   CLOUDINARY: {
-        CLOUDINARY_CLOUD_NAME: string;
-        CLOUDINARY_API_KEY: string;
-        CLOUDINARY_API_SECRET: string;
-    };
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+  };
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -43,9 +43,9 @@ const loadEnvVariables = (): EnvConfig => {
     "GOOGLE_CALLBACK_URL",
     "EXPRESS_SESSION_SECRET",
     "FRONTEND_URL",
-     "CLOUDINARY_CLOUD_NAME",
-        "CLOUDINARY_API_KEY",
-        "CLOUDINARY_API_SECRET",
+    "CLOUDINARY_CLOUD_NAME",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
   ];
 
   requiredEnvVariables.forEach((key) => {
@@ -70,11 +70,11 @@ const loadEnvVariables = (): EnvConfig => {
     GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
     EXPRESS_SESSION_SECRET: process.env.EXPRESS_SESSION_SECRET as string,
     FRONTEND_URL: process.env.FRONTEND_URL as string,
-         CLOUDINARY: {
-            CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
-            CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
-            CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
-        },
+    CLOUDINARY: {
+      CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+      CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+      CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+    },
   };
 };
 
